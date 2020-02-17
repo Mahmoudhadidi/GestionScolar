1341b0a94d0cd8429db1c362f4dd5d163aab1032
bechirkhalilachour <bechirkhalil.achour@esprit.tn>
2020-02-17 22:28:34 +0100

    les entité absences

---
+++ src/com/esprit/Entite/absence_employe.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.Entite;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class absence_employe {
+     private int id_absence_e;
+     private String type_absence;
+     private int id_employe;
+    
+
+    
+
+    public absence_employe(String type_absence, int id_employe) {
+        this.type_absence = type_absence;
+        this.id_employe = id_employe;
+    }
+
+    public absence_employe(int id_absence_e, String type_absence, int id_employe) {
+        this.id_absence_e = id_absence_e;
+        this.type_absence = type_absence;
+        this.id_employe = id_employe;
+    }
+
+    public absence_employe() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public int getId_absence_e() {
+        return id_absence_e;
+    }
+
+    public String getType_absence() {
+        return type_absence;
+    }
+
+    public int getId_employe() {
+        return id_employe;
+    }
+
+    public void setId_absence_e(int id_absence_e) {
+        this.id_absence_e = id_absence_e;
+    }
+
+    public void setType_absence(String type_absence) {
+        this.type_absence = type_absence;
+    }
+
+    public void setId_employe(int id_employe) {
+        this.id_employe = id_employe;
+    }
+
+    @Override
+    public String toString() {
+        return "absence_employe{" + "id_absence_e=" + id_absence_e + ", type_absence=" + type_absence + ", id_employe=" + id_employe + '}';
+    }
+    
+    
+ 
+    
+    
+}
